924e809d50b9107bdc6f8d8ff2e781ad7823fefb
Nunor <161338059+NunoRVVS@users.noreply.github.com>
2025-10-29 22:58:31 +0000

    components elements names updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./sections/Hero.jsx"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx"
-import NavBar from "./components/Navbar.jsx"
+import NavBar from "./components/NavBar.jsx"
 import LogoSection from "./sections/LogoSection.jsx"
 import FeatureCards from "./sections/FeatureCards.jsx"
 import NewExperienceSection from "./sections/NewExperienceSection.jsx"
